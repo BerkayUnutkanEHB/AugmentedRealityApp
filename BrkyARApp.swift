c6b550709641902c676c600e028827e799d7f1b5
BerkayUnutkanEHB <berkay.unutkan@student.ehb.be>
2024-11-07 10:28:51 +0100

    Initial commit

---
+++ BrkyARApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrkyARApp.swift
+//  BrkyAR
+//
+//  Created by Berkay Unutkan on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BrkyARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrkyAR
+//
+//  Created by Berkay Unutkan on 07/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
